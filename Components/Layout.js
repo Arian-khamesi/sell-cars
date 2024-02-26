5168332f1ec7c5eb20da35f7cc27115f82254fbb
aryaun xames <ariankhamesi@gmail.com>
2024-02-26 12:38:57 +0330

    create header and footer in layout component and styling them

---
+++ Components/Layout.js
@@ -0,0 +1,24 @@
+import Link from "next/link"
+import styles from "./Layout.module.css"
+export default function Layout({ children }) {
+    return (
+        <>
+            <header className={styles.header}>
+              <Link href={"/"}>
+              <h1 className={styles.text}>
+                    CarBaam
+                </h1>
+                <p className={styles.text}>
+                    Choose And Buy Your car
+                </p>
+              </Link>
+            </header>
+            <div className={styles.container}>
+            {children}
+            </div>
+            <footer className={styles.footer}>
+                <span className={styles.foot_span}>next js practice</span>CarBaam project &copy;
+            </footer>
+        </>
+    )
+}
